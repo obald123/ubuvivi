44e0eb5e6c9ddc0fac4093505457244958ac9e7c
obald123 <simugomwaobald250@gmail.com>
2026-05-27 17:45:58 +0200

    fortify routes fixed

--- ubuvivi-road-destination-main/config/fortify.php
+++ ubuvivi-road-destination-main/config/fortify.php
@@ -78,58 +78,17 @@ return [
 
     'domain' => null,
 
-    /*
-    |--------------------------------------------------------------------------
-    | Fortify Routes Middleware
-    |--------------------------------------------------------------------------
-    |
-    | Here you may specify which middleware Fortify will assign to the routes
-    | that it registers with the application. If necessary, you may change
-    | these middleware but typically this provided default is preferred.
-    |
-    */
-
     'middleware' => ['web'],
 
-    /*
-    |--------------------------------------------------------------------------
-    | Rate Limiting
-    |--------------------------------------------------------------------------
-    |
-    | By default, Fortify will throttle logins to five requests per minute for
-    | every email and IP address combination. However, if you would like to
-    | specify a custom rate limiter to call then you may specify it here.
-    |
-    */
 
     'limiters' => [
         'login' => 'login',
         'two-factor' => 'two-factor',
     ],
 
-    /*
-    |--------------------------------------------------------------------------
-    | Register View Routes
-    |--------------------------------------------------------------------------
-    |
-    | Here you may specify if the routes returning views should be disabled as
-    | you may not need them when building your own application. This may be
-    | especially true if you're writing a custom single-page application.
-    |
-    */
 
-    'views' => false,
+    'views' => true,
 
-    /*
-    |--------------------------------------------------------------------------
-    | Features
-    |--------------------------------------------------------------------------
-    |
-    | Some of the Fortify features are optional. You may disable the features
-    | by removing them from this array. You're free to only remove some of
-    | these features or you can even remove all of these if you need to.
-    |
-    */
 
     'features' => [
         Features::registration(),
